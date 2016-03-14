93bab7a8cbace051056d78a44cba697041910b4c
Marco Tezzele <marcotez@gmail.com>
2016-03-14 15:32:15 +0100

    parse method and test cleaning

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -16,25 +16,15 @@ class TestFileHandler(TestCase):
 
 	
 	def test_base_class_parse(self):
-		works = True
-		try:
-			file_handler = fh.FileHandler()
+		file_handler = fh.FileHandler()
+		with self.assertRaises(NotImplementedError):
 			file_handler.parse()
-			works = False
-		except:
-			pass
-		assert works
 
 
 	def test_base_class_write(self):
-		works = True
-		try:
-			file_handler = fh.FileHandler()
+		file_handler = fh.FileHandler()
+		with self.assertRaises(NotImplementedError):
 			file_handler.write()
-			works = False
-		except:
-			pass
-		assert works
 	
 
 	def test_unv_file_member(self):
@@ -52,42 +42,42 @@ class TestFileHandler(TestCase):
 		assert unv_handler.extension == 'unv'
 
 
-	def test_unv_passing_filename_type(self):
-		works = False
-		try:
-			unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
-			works = True
-		except:
-			pass
-		assert works
-
-
 	def test_unv_failing_filename_type(self):
-		works = True
-		try:
-			unv_handler = fh.UnvHandler(test_square.iges)
-			works = False
-		except:
-			pass
-		assert works
+		with self.assertRaises(TypeError):
+			unv_handler = fh.UnvHandler(3)
 
 
-	def test_unv_passing_check_extension(self):
-		works = False
-		try:
-			unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
-			works = True
-		except:
-			pass
-		assert works
-
+	def test_unv_instantiation(self):
+		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
+	
 
 	def test_unv_failing_check_extension(self):
-		works = True
-		try:
+		with self.assertRaises(ValueError):
 			unv_handler = fh.UnvHandler('tests/test_datasets/test_square.iges')
-			works = False
-		except:
-			pass
-		assert works
+
+
+	def test_unv_parse_shape(self):
+		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
+		mesh_points = unv_handler.parse()
+		assert mesh_points.shape == (256, 3)
+
+
+	def test_unv_parse_coords_1(self):
+		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
+		mesh_points = unv_handler.parse()
+		np.testing.assert_almost_equal(abs(mesh_points[33][0]), 1.0)
+
+
+	def test_unv_parse_coords_2(self):
+		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
+		mesh_points = unv_handler.parse()
+		np.testing.assert_almost_equal(abs(mesh_points[178][1]), 0.4)
+
+
+	def test_unv_parse_coords_3(self):
+		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
+		mesh_points = unv_handler.parse()
+		np.testing.assert_almost_equal(abs(mesh_points[100][2]), 0.0)		
+
+
 

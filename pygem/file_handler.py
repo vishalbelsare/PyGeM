66ad009c4828ef4d280a0490c701446d45f38b13
Marco Tezzele <marcotez@gmail.com>
2016-03-16 09:58:12 +0100

    write method in unv parser and tests

--- pygem/file_handler.py
+++ pygem/file_handler.py
@@ -8,14 +8,12 @@ class FileHandler(object):
 	"""
 	A base class for file handling.
 
-	:cvar file: 
 	:cvar string filename: name of the file to be processed
 	:cvar string extension: extension of the file to be processed
 
 	.. todo:: Add virtual methods like parse and write
 	"""
 	def __init__(self):
-		self.file = None
 		self.filename = None
 		self.extension = None
 
@@ -23,6 +21,7 @@ class FileHandler(object):
 	def _extract_extension(self):
 		"""
 		It is used in child classes.
+		
 		.. todo:: DOCS
 		"""
 		ext = self.filename.split('.')[-1].lower()
@@ -31,10 +30,10 @@ class FileHandler(object):
 
 	def _check_extension(self, extension):
 		"""
-		Internal function that checks that the member extension is equal to 
+		Internal function that checks that the member extension is equal to
 		a given extension.
 		It is used in child classes.
-		
+
 		:param string extension: file extension to check.
 		"""
 		if not self.extension == extension:
@@ -48,17 +47,19 @@ class FileHandler(object):
 
 		Not implemented, it has to be implemented in subclasses.
 		"""
-		raise NotImplementedError("Subclass must implement abstract method " + self.__class__.__name__ + ".parse")
+		raise NotImplementedError("Subclass must implement abstract method " \
+			+ self.__class__.__name__ + ".parse")
 
 
-	def write(self):
+	def write(self, mesh_points, outfile):
 		"""
 		Abstract method to write a specific file.
 
 		Not implemented, it has to be implemented in subclasses.
 		"""
-		raise NotImplementedError("Subclass must implement abstract method " + self.__class__.__name__ + ".write")
-		
+		raise NotImplementedError("Subclass must implement abstract method " \
+			+ self.__class__.__name__ + ".write")
+
 
 
 class UnvHandler(FileHandler):
@@ -72,7 +73,7 @@ class UnvHandler(FileHandler):
 
 		if not isinstance(filename, basestring):
 			raise TypeError("filename must be a string")
-		
+
 		self.filename = filename
 		self.extension = self._extract_extension()
 		self._check_extension('unv')
@@ -81,16 +82,16 @@ class UnvHandler(FileHandler):
 	def parse(self):
 		"""
 		Method to parse the `filename`. It returns a matrix with all the coordinates.
-	
+
 		:return: mesh_points: it is a `n_points`-by-3 matrix containing the coordinates of
-			the points of the mesh.
+			the points of the mesh
 		:rtype: float ndarray
-	
+
 		.. todo::
 
 			- specify when it works
 		"""
-	
+
 		input_file = open(self.filename, 'r')
 		nline = 0
 		while True:
@@ -103,31 +104,61 @@ class UnvHandler(FileHandler):
 				nline += 1
 				if section_id == '2411':
 					count = 0
-					while not(input_file.readline().startswith('    -1')):
+					while not input_file.readline().startswith('    -1'):
 						count += 1
 					start_line = nline + 2
 					last_line = start_line + count
 				else:
-					while not(input_file.readline().startswith('    -1')):
+					while not input_file.readline().startswith('    -1'):
 						nline += 1
-		
+
 		input_file.close()
 
 		n_points = count/2
 		mesh_points = np.zeros(shape=(n_points, 3))
-	
+
 		nline = 0
 		i = 0
 		with open(self.filename, 'r') as input_file:
 			for line in input_file:
 				nline += 1
-				if nline % 2 == 1 and nline >= start_line and nline < last_line:
+				if nline % 2 == 1 and nline > start_line and nline < last_line:
 					line = line.strip()
 					j = 0
 					for number in line.split():
 						mesh_points[i][j] = float(number)
 						j += 1
 					i += 1
-	
-		return mesh_points	
-	
+
+		return mesh_points
+
+
+	def write(self, mesh_points, outfile):
+		"""
+		Writes a unv file, called outfile, copying all the lines from self.filename but
+		the coordinates. mesh_points is a matrix that contains the new coordinates to
+		write in the unv file.
+
+		:param ndarray mesh_points: it is a `n_points`-by-3 matrix containing
+			the coordinates of the points of the mesh
+
+		.. todo:: DOCS
+		"""
+		if not isinstance(outfile, basestring):
+			raise TypeError("outfile must be a string")
+
+		n_points = mesh_points.shape[0]
+		nrow = 0
+		i = 0
+		with open(self.filename, 'r') as input_file, open(outfile, 'w') as output_file:
+			for line in input_file:
+				nrow += 1
+				if nrow % 2 == 1 and nrow > 20 and nrow <= (20 + n_points * 2):
+					for j in range(0, 3):
+						output_file.write('   ' + str(mesh_points[i][j]))
+					output_file.write('\n')
+					i += 1
+				elif nrow > 17:
+					output_file.write(line)
+
+

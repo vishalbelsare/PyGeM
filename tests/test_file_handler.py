d2a90fec3dbd0871a9a23a0911aac76d4b8f0b91
Marco Tezzele <marcotez@gmail.com>
2016-03-26 16:40:35 +0100

    changed init file handler and members

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -10,9 +10,14 @@ import os
 class TestFileHandler(TestCase):
 
 
-	def test_base_class_filename(self):
+	def test_base_class_infile(self):
 		file_handler = fh.FileHandler()
-		assert file_handler.filename == None
+		assert file_handler.infile == None
+
+
+	def test_base_class_outfile(self):
+		file_handler = fh.FileHandler()
+		assert file_handler.outfile == None
 
 
 	def test_base_class_extension(self):
@@ -23,7 +28,7 @@ class TestFileHandler(TestCase):
 	def test_base_class_parse(self):
 		file_handler = fh.FileHandler()
 		with self.assertRaises(NotImplementedError):
-			file_handler.parse()
+			file_handler.parse('input')
 
 
 	def test_base_class_write(self):
@@ -34,76 +39,112 @@ class TestFileHandler(TestCase):
 
 
 	# UNV tests
-	def test_unv_filename_member(self):
-		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
-		assert unv_handler.filename == 'tests/test_datasets/test_square.unv'
-	
-
-	def test_unv_extension_member(self):
-		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
-		assert unv_handler.extension == 'unv'
-
-
-	def test_unv_failing_filename_type(self):
-		with self.assertRaises(TypeError):
-			unv_handler = fh.UnvHandler(3)
-
-
 	def test_unv_instantiation(self):
-		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
+		unv_handler = fh.UnvHandler()
 	
 
-	def test_unv_failing_check_extension(self):
+	def test_unv_default_infile_member(self):
+		unv_handler = fh.UnvHandler()
+		assert unv_handler.infile == None
+
+
+	def test_unv_default_outfile_member(self):
+		unv_handler = fh.UnvHandler()
+		assert unv_handler.outfile == None
+
+
+	def test_unv_default_extension_member(self):
+		unv_handler = fh.UnvHandler()
+		assert unv_handler.extension == 'unv'
+	
+
+	def test_unv_parse_failing_filename_type(self):
+		unv_handler = fh.UnvHandler()
+		with self.assertRaises(TypeError):
+			mesh_points = unv_handler.parse(5.2)
+
+	
+	def test_unv_parse_failing_check_extension(self):
+		unv_handler = fh.UnvHandler()
 		with self.assertRaises(ValueError):
-			unv_handler = fh.UnvHandler('tests/test_datasets/test_square.iges')
+			mesh_points = unv_handler.parse('tests/test_datasets/test_square.iges')
+
+
+	def test_unv_parse_infile(self):
+		unv_handler = fh.UnvHandler()
+		mesh_points = unv_handler.parse('tests/test_datasets/test_square.unv')
+		assert unv_handler.infile == 'tests/test_datasets/test_square.unv'
 
 
 	def test_unv_parse_shape(self):
-		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
-		mesh_points = unv_handler.parse()
+		unv_handler = fh.UnvHandler()
+		mesh_points = unv_handler.parse('tests/test_datasets/test_square.unv')
 		assert mesh_points.shape == (256, 3)
 
 
 	def test_unv_parse_coords_1(self):
-		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
-		mesh_points = unv_handler.parse()
+		unv_handler = fh.UnvHandler()
+		mesh_points = unv_handler.parse('tests/test_datasets/test_square.unv')
 		np.testing.assert_almost_equal(mesh_points[33][0], 1.0)
 
 
 	def test_unv_parse_coords_2(self):
-		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
-		mesh_points = unv_handler.parse()
+		unv_handler = fh.UnvHandler()
+		mesh_points = unv_handler.parse('tests/test_datasets/test_square.unv')
 		np.testing.assert_almost_equal(mesh_points[178][1], 0.4)
 
 
 	def test_unv_parse_coords_3(self):
-		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
-		mesh_points = unv_handler.parse()
+		unv_handler = fh.UnvHandler()
+		mesh_points = unv_handler.parse('tests/test_datasets/test_square.unv')
 		np.testing.assert_almost_equal(mesh_points[100][2], 0.0)
 
 
 	def test_unv_parse_coords_4(self):
-		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
-		mesh_points = unv_handler.parse()
+		unv_handler = fh.UnvHandler()
+		mesh_points = unv_handler.parse('tests/test_datasets/test_square.unv')
 		np.testing.assert_almost_equal(mesh_points[0][0], 0.0)
 		
 		
 	def test_unv_parse_coords_5(self):
-		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
-		mesh_points = unv_handler.parse()
+		unv_handler = fh.UnvHandler()
+		mesh_points = unv_handler.parse('tests/test_datasets/test_square.unv')
 		np.testing.assert_almost_equal(mesh_points[-1][2], 0.0)		
 
 
-	def test_unv_write_failing_outfile_type(self):
-		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
-		mesh_points = unv_handler.parse()
+	def test_unv_write_failing_filename_type(self):
+		unv_handler = fh.UnvHandler()
+		mesh_points = unv_handler.parse('tests/test_datasets/test_square.unv')
 		with self.assertRaises(TypeError):
-			unv_handler.write(mesh_points, 3)
- 
+			unv_handler.write(mesh_points, -2)
+
+
+	def test_unv_write_failing_check_extension(self):
+		unv_handler = fh.UnvHandler()
+		mesh_points = unv_handler.parse('tests/test_datasets/test_square.unv')
+		with self.assertRaises(ValueError):
+			unv_handler.write(mesh_points, 'tests/test_datasets/test_square.iges')
+
+
+	def test_unv_write_failing_infile_instantiation(self):
+		unv_handler = fh.UnvHandler()
+		mesh_points = np.zeros((20, 3))
+		with self.assertRaises(RuntimeError):
+ 			unv_handler.write(mesh_points, 'tests/test_datasets/test_square_out.unv')
+
 
 	def test_unv_write_outfile(self):
-		unv_handler = fh.UnvHandler('tests/test_datasets/test_square.unv')
-		mesh_points = unv_handler.parse()
+		unv_handler = fh.UnvHandler()
+		mesh_points = unv_handler.parse('tests/test_datasets/test_square.unv')
+		outfilename = 'tests/test_datasets/test_square_out.unv'
+		unv_handler.write(mesh_points, outfilename)
+		assert unv_handler.outfile == outfilename
+		os.remove(outfilename)
+
+
+	def test_unv_write_comparison(self):
+		unv_handler = fh.UnvHandler()
+		mesh_points = unv_handler.parse('tests/test_datasets/test_square.unv')
 		mesh_points[0][0] = 2.2
 		mesh_points[5][1] = 4.3
 		mesh_points[9][2] = 0.5
@@ -120,76 +161,112 @@ class TestFileHandler(TestCase):
 
 
 	# STL tests
-	def test_stl_filename_member(self):
-		stl_handler = fh.StlHandler('tests/test_datasets/test_sphere.stl')
-		assert stl_handler.filename == 'tests/test_datasets/test_sphere.stl'
-	
-
-	def test_stl_extension_member(self):
-		stl_handler = fh.StlHandler('tests/test_datasets/test_sphere.stl')
-		assert stl_handler.extension == 'stl'
-
-
-	def test_stl_failing_filename_type(self):
-		with self.assertRaises(TypeError):
-			stl_handler = fh.StlHandler(3)
-
-
 	def test_stl_instantiation(self):
-		stl_handler = fh.StlHandler('tests/test_datasets/test_sphere.stl')
+		stl_handler = fh.StlHandler()
 	
 
-	def test_stl_failing_check_extension(self):
+	def test_stl_default_infile_member(self):
+		stl_handler = fh.StlHandler()
+		assert stl_handler.infile == None
+
+
+	def test_stl_default_outfile_member(self):
+		stl_handler = fh.StlHandler()
+		assert stl_handler.outfile == None
+
+
+	def test_stl_default_extension_member(self):
+		stl_handler = fh.StlHandler()
+		assert stl_handler.extension == 'stl'
+	
+
+	def test_stl_parse_failing_filename_type(self):
+		stl_handler = fh.StlHandler()
+		with self.assertRaises(TypeError):
+			mesh_points = stl_handler.parse(5.2)
+
+	
+	def test_stl_parse_failing_check_extension(self):
+		stl_handler = fh.StlHandler()
 		with self.assertRaises(ValueError):
-			stl_handler = fh.StlHandler('tests/test_datasets/test_square.iges')
+			mesh_points = stl_handler.parse('tests/test_datasets/test_square.iges')
+
+
+	def test_stl_parse_infile(self):
+		stl_handler = fh.StlHandler()
+		mesh_points = stl_handler.parse('tests/test_datasets/test_sphere.stl')
+		assert stl_handler.infile == 'tests/test_datasets/test_sphere.stl'
 
 
 	def test_stl_parse_shape(self):
-		stl_handler = fh.StlHandler('tests/test_datasets/test_sphere.stl')
-		mesh_points = stl_handler.parse()
+		stl_handler = fh.StlHandler()
+		mesh_points = stl_handler.parse('tests/test_datasets/test_sphere.stl')
 		assert mesh_points.shape == (7200, 3)
 
 
 	def test_stl_parse_coords_1(self):
-		stl_handler = fh.StlHandler('tests/test_datasets/test_sphere.stl')
-		mesh_points = stl_handler.parse()
+		stl_handler = fh.StlHandler()
+		mesh_points = stl_handler.parse('tests/test_datasets/test_sphere.stl')
 		np.testing.assert_almost_equal(mesh_points[33][0], -21.31975937)
 
 
 	def test_stl_parse_coords_2(self):
-		stl_handler = fh.StlHandler('tests/test_datasets/test_sphere.stl')
-		mesh_points = stl_handler.parse()
+		stl_handler = fh.StlHandler()
+		mesh_points = stl_handler.parse('tests/test_datasets/test_sphere.stl')
 		np.testing.assert_almost_equal(mesh_points[1708][1], 2.58431911)
 
 
 	def test_stl_parse_coords_3(self):
-		stl_handler = fh.StlHandler('tests/test_datasets/test_sphere.stl')
-		mesh_points = stl_handler.parse()
+		stl_handler = fh.StlHandler()
+		mesh_points = stl_handler.parse('tests/test_datasets/test_sphere.stl')
 		np.testing.assert_almost_equal(mesh_points[3527][2], -2.47207999)
 
 
 	def test_stl_parse_coords_4(self):
-		stl_handler = fh.StlHandler('tests/test_datasets/test_sphere.stl')
-		mesh_points = stl_handler.parse()
+		stl_handler = fh.StlHandler()
+		mesh_points = stl_handler.parse('tests/test_datasets/test_sphere.stl')
 		np.testing.assert_almost_equal(mesh_points[0][0], -21.31975937)	
 
 
 	def test_stl_parse_coords_5(self):
-		stl_handler = fh.StlHandler('tests/test_datasets/test_sphere.stl')
-		mesh_points = stl_handler.parse()
+		stl_handler = fh.StlHandler()
+		mesh_points = stl_handler.parse('tests/test_datasets/test_sphere.stl')
 		np.testing.assert_almost_equal(mesh_points[-1][2], -39.05963898)		
 
 
-	def test_stl_write_failing_outfile_type(self):
-		stl_handler = fh.StlHandler('tests/test_datasets/test_sphere.stl')
-		mesh_points = stl_handler.parse()
+	def test_stl_write_failing_filename_type(self):
+		stl_handler = fh.StlHandler()
+		mesh_points = stl_handler.parse('tests/test_datasets/test_sphere.stl')
 		with self.assertRaises(TypeError):
-			stl_handler.write(mesh_points, 3)
- 
+			stl_handler.write(mesh_points, 4.)
+
+
+	def test_stl_write_failing_check_extension(self):
+		stl_handler = fh.StlHandler()
+		mesh_points = stl_handler.parse('tests/test_datasets/test_sphere.stl')
+		with self.assertRaises(ValueError):
+			stl_handler.write(mesh_points, 'tests/test_datasets/test_square.iges')
+
+
+	def test_stl_write_failing_infile_instantiation(self):
+		stl_handler = fh.StlHandler()
+		mesh_points = np.zeros((40, 3))
+		with self.assertRaises(RuntimeError):
+ 			stl_handler.write(mesh_points, 'tests/test_datasets/test_sphere_out.stl')
+
 
 	def test_stl_write_outfile(self):
-		stl_handler = fh.StlHandler('tests/test_datasets/test_sphere.stl')
-		mesh_points = stl_handler.parse()
+		stl_handler = fh.StlHandler()
+		mesh_points = stl_handler.parse('tests/test_datasets/test_sphere.stl')
+		outfilename = 'tests/test_datasets/test_sphere_out.stl'
+		stl_handler.write(mesh_points, outfilename)
+		assert stl_handler.outfile == outfilename
+		os.remove(outfilename)
+
+
+	def test_stl_write_comparison(self):
+		stl_handler = fh.StlHandler()
+		mesh_points = stl_handler.parse('tests/test_datasets/test_sphere.stl')
 		mesh_points[0] = [-40.2, -20.5, 60.9]
 		mesh_points[1] = [-40.2, -10.5, 60.9]
 		mesh_points[2] = [-40.2, -10.5, 60.9]
@@ -209,72 +286,118 @@ class TestFileHandler(TestCase):
 
 
 	def test_stl_plot_failing_outfile_type(self):
-		stl_handler = fh.StlHandler('tests/test_datasets/test_sphere.stl')
+		stl_handler = fh.StlHandler()
 		with self.assertRaises(TypeError):
 			stl_handler.plot(plot_file=3)
 			
 	
 	# openFOAM tests
-	def test_open_foam_filename_member(self):
-		open_foam_handler = fh.OpenFoamHandler('tests/test_datasets/test_openFOAM')
-		assert open_foam_handler.filename == 'tests/test_datasets/test_openFOAM'
-
-
-	def test_open_foam_failing_filename_type(self):
-		with self.assertRaises(TypeError):
-			open_foam_handler = fh.OpenFoamHandler(3)
-
-
 	def test_open_foam_instantiation(self):
-		open_foam_handler = fh.OpenFoamHandler('tests/test_datasets/test_openFOAM')
+		open_foam_handler = fh.OpenFoamHandler()
+	
+
+	def test_open_foam_default_infile_member(self):
+		open_foam_handler = fh.OpenFoamHandler()
+		assert open_foam_handler.infile == None
+
+
+	def test_open_foam_default_outfile_member(self):
+		open_foam_handler = fh.OpenFoamHandler()
+		assert open_foam_handler.outfile == None
+
+
+	def test_open_foam_default_extension_member(self):
+		open_foam_handler = fh.OpenFoamHandler()
+		assert open_foam_handler.extension == ''
+	
+
+	def test_open_foam_parse_failing_filename_type(self):
+		open_foam_handler = fh.OpenFoamHandler()
+		with self.assertRaises(TypeError):
+			mesh_points = open_foam_handler.parse(.2)
+
+	
+	def test_open_foam_parse_failing_check_extension(self):
+		open_foam_handler = fh.OpenFoamHandler()
+		with self.assertRaises(ValueError):
+			mesh_points = open_foam_handler.parse('tests/test_datasets/test_square.iges')
+
+
+	def test_open_foam_parse_infile(self):
+		open_foam_handler = fh.OpenFoamHandler()
+		mesh_points = open_foam_handler.parse('tests/test_datasets/test_openFOAM')
+		assert open_foam_handler.infile == 'tests/test_datasets/test_openFOAM'
 
 
 	def test_open_foam_parse_shape(self):
-		open_foam_handler = fh.OpenFoamHandler('tests/test_datasets/test_openFOAM')
-		mesh_points = open_foam_handler.parse()
+		open_foam_handler = fh.OpenFoamHandler()
+		mesh_points = open_foam_handler.parse('tests/test_datasets/test_openFOAM')
 		assert mesh_points.shape == (21812, 3)
 
 
 	def test_open_foam_parse_coords_1(self):
-		open_foam_handler = fh.OpenFoamHandler('tests/test_datasets/test_openFOAM')
-		mesh_points = open_foam_handler.parse()
+		open_foam_handler = fh.OpenFoamHandler()
+		mesh_points = open_foam_handler.parse('tests/test_datasets/test_openFOAM')
 		np.testing.assert_almost_equal(mesh_points[33][0], 1.42254)
 
 
 	def test_open_foam_parse_coords_2(self):
-		open_foam_handler = fh.OpenFoamHandler('tests/test_datasets/test_openFOAM')
-		mesh_points = open_foam_handler.parse()
+		open_foam_handler = fh.OpenFoamHandler()
+		mesh_points = open_foam_handler.parse('tests/test_datasets/test_openFOAM')
 		np.testing.assert_almost_equal(mesh_points[1708][1], -3.13059)
 
 
 	def test_open_foam_parse_coords_3(self):
-		open_foam_handler = fh.OpenFoamHandler('tests/test_datasets/test_openFOAM')
-		mesh_points = open_foam_handler.parse()
+		open_foam_handler = fh.OpenFoamHandler()
+		mesh_points = open_foam_handler.parse('tests/test_datasets/test_openFOAM')
 		np.testing.assert_almost_equal(mesh_points[3527][2], .0)
 
 
 	def test_open_foam_parse_coords_4(self):
-		open_foam_handler = fh.OpenFoamHandler('tests/test_datasets/test_openFOAM')
-		mesh_points = open_foam_handler.parse()
+		open_foam_handler = fh.OpenFoamHandler()
+		mesh_points = open_foam_handler.parse('tests/test_datasets/test_openFOAM')
 		np.testing.assert_almost_equal(mesh_points[0][0], -17.5492)	
 
 
 	def test_open_foam_parse_coords_5(self):
-		open_foam_handler = fh.OpenFoamHandler('tests/test_datasets/test_openFOAM')
-		mesh_points = open_foam_handler.parse()
+		open_foam_handler = fh.OpenFoamHandler()
+		mesh_points = open_foam_handler.parse('tests/test_datasets/test_openFOAM')
 		np.testing.assert_almost_equal(mesh_points[-1][2], 0.05)		
 
 
-	def test_open_foam_write_failing_outfile_type(self):
-		open_foam_handler = fh.OpenFoamHandler('tests/test_datasets/test_openFOAM')
-		mesh_points = open_foam_handler.parse()
+	def test_open_foam_write_failing_filename_type(self):
+		open_foam_handler = fh.OpenFoamHandler()
+		mesh_points = open_foam_handler.parse('tests/test_datasets/test_openFOAM')
 		with self.assertRaises(TypeError):
-			open_foam_handler.write(mesh_points, 3)
- 
+			open_foam_handler.write(mesh_points, -1.)
+
+
+	def test_open_foam_write_failing_check_extension(self):
+		open_foam_handler = fh.OpenFoamHandler()
+		mesh_points = open_foam_handler.parse('tests/test_datasets/test_openFOAM')
+		with self.assertRaises(ValueError):
+			open_foam_handler.write(mesh_points, 'tests/test_datasets/test_square.iges')
+
+
+	def test_open_foam_write_failing_infile_instantiation(self):
+		open_foam_handler = fh.OpenFoamHandler()
+		mesh_points = np.zeros((40, 3))
+		with self.assertRaises(RuntimeError):
+ 			open_foam_handler.write(mesh_points, 'tests/test_datasets/test_openFOAM_out')
+
 
 	def test_open_foam_write_outfile(self):
-		open_foam_handler = fh.OpenFoamHandler('tests/test_datasets/test_openFOAM')
-		mesh_points = open_foam_handler.parse()
+		open_foam_handler = fh.OpenFoamHandler()
+		mesh_points = open_foam_handler.parse('tests/test_datasets/test_openFOAM')
+		outfilename = 'tests/test_datasets/test_openFOAM_out'
+		open_foam_handler.write(mesh_points, outfilename)
+		assert open_foam_handler.outfile == outfilename
+		os.remove(outfilename)
+
+
+	def test_open_foam_write_comparison(self):
+		open_foam_handler = fh.OpenFoamHandler()
+		mesh_points = open_foam_handler.parse('tests/test_datasets/test_openFOAM')
 		mesh_points[0] = [-14.,  1.55, 0.2]
 		mesh_points[1] = [-14.3, 2.55, 0.3]
 		mesh_points[2] = [-14.3, 2.55, 0.3]

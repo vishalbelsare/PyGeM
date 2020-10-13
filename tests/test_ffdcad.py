9baf83226494b1f7978a6d3fbd55d3e952e75e8d
Andrea Mola <amola@gaia.sissa.it>
2020-10-13 18:36:00 +0200

    Partially fixed step export and added a related test

--- tests/test_ffdcad.py
+++ tests/test_ffdcad.py
@@ -3,10 +3,21 @@ import os
 from unittest import TestCase
 
 import numpy as np
+import re
 from OCC.Core.TopoDS import TopoDS_Shape
 
 from pygem.cad import FFD
 from pygem.cad import CADDeformation
+
+
+
+
+def floatstrcmp(left, right):
+   try:
+      return cmp(float(left), float(right))
+   except ValueError:
+      return cmp(left, right)
+
 
 class TestFFDCAD(TestCase):
 
@@ -44,3 +55,21 @@ class TestFFDCAD(TestCase):
                  cre_ = np.asarray(cre[i].split(',')[:-1], dtype=float)
                  np.testing.assert_array_almost_equal(cre_, ref_, decimal=6)
         self.addCleanup(os.remove, 'test_pipe_hollow_result.iges')
+        
+        
+        
+    def test_ffd_step_pipe_mod_through_files(self):
+        ffd = FFD(None,30,30,30,1e-4)
+        ffd.read_parameters(
+            filename='tests/test_datasets/parameters_test_ffd_iges.prm')
+        ffd('tests/test_datasets/test_pipe.step', 'test_pipe_result.step')
+        with open('test_pipe_result.step', "r") as created, \
+             open('tests/test_datasets/test_pipe_out_true.step', "r") as reference:
+             ref = reference.readlines()[92:]
+             cre = created.readlines()[92:]
+             self.assertEqual(len(ref),len(cre))
+             for i in range(len(cre)):
+                 ref_ = np.asarray(re.findall(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?", ref[i]),dtype=float)
+                 cre_ = np.asarray(re.findall(r"[+-]? *(?:\d+(?:\.\d*)?|\.\d+)(?:[eE][+-]?\d+)?", cre[i]),dtype=float)
+                 np.testing.assert_array_almost_equal(cre_, ref_, decimal=6)
+        self.addCleanup(os.remove, 'test_pipe_result.step')
